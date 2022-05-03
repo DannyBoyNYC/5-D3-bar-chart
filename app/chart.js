be00ec780e7498c7796f9f0dadd9d317158a6848
Daniel Deverell <daniel.deverell@gmail.com>
2022-05-03 19:11:36 -0400

    done

--- app/chart.js
+++ app/chart.js
@@ -1,4 +1,74 @@
 async function drawBars() {
-  // your code goes here
+  const dataset = await d3.json("./data/my_weather_data.json");
+  const xAccessor = (d) => d.humidity;
+  const yAccessor = (d) => d.length;
+
+  const width = 600;
+
+  let dimensions = {
+    width,
+    height: width * 0.6,
+    margin: {
+      top: 30,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    },
+  };
+
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
+
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice();
+
+  console.log(xScale.domain());
+  console.log(xScale.range());
+
+  const binsGenerator = d3
+    .bin()
+    .domain(xScale.domain())
+    .value(xAccessor)
+    .thresholds(12);
+
+  const bins = binsGenerator(dataset);
+
+  console.log(bins);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(bins, yAccessor)])
+    .range([dimensions.boundedHeight, 0])
+    .nice();
+
+  const binsGroup = bounds.append("g");
+  const binGroups = binsGroup.selectAll("g").data(bins).join("g");
+  const barPadding = 1;
+  const barRects = binGroups
+    .append("rect")
+    .attr("x", (d) => xScale(d.x0) + barPadding / 2)
+    .attr("y", (d) => yScale(yAccessor(d)))
+    .attr("width", (d) => d3.max([0, xScale(d.x1) - xScale(d.x0) - barPadding]))
+    .attr("height", (d) => dimensions.boundedHeight - yScale(yAccessor(d)))
+    .attr("fill", "cornflowerblue");
 }
+
 drawBars();
